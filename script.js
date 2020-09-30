35b57fc1c4e90a4417f946d3d8accfa6bfb933ab
Will142 <will142@hotmail.fr>
2020-09-30 17:03:30 +0200

    fix header bug start new event card

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ document.body.addEventListener('click', (e) => {
 mobileBurgerButton.addEventListener('click', () => {
     mobileBurger.style.display = 'flex';
 });
-
 /*JS Home Page */
 
 
@@ -31,41 +30,3 @@ const eventList = [
  
 ];
 
-function createCard(title, imageUrl) {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    cards.appendChild(card);
-  
-    const cardHeader = document.createElement("div");
-    cardHeader.classList.add("card-header");
-    card.appendChild(cardHeader);
-  
-    const cardImg = document.createElement("div");
-    cardImg.style.backgroundImage = `url(${imageUrl})`;
-    cardImg.classList.add("card-img");
-    cardHeader.appendChild(cardImg);
-  
-    // Step1: Create the cardBody div, add the class card-body and add it to the card
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-    cardHeader.appendChild(cardBody);
-  
-    // Step2: Create the cardTitle h2, add the class card-title,
-    // set the text inside the tag to the "title" parameter of this function
-    // and add it to the cardBody
-    const cardTitle = document.createElement("h2");
-    cardTitle.classList.add("card-title");
-    cardTitle.innerHTML = `${title}`;
-    cardBody.appendChild(cardTitle);
-  
-    
-  /*  const cardButton = document.createElement("button");
-    cardButton.classList.add("card-button");
-    cardButton.innerHTML = "Adopt Now";
-    cardBody.appendChild(cardButton);*/
-  }
-
-  
-  for (let i = 0; i < eventList.length; i++) {
-    createCard(eventList[i].name, eventList[i].picture);
-  }
