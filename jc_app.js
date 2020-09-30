e8a28078ca8a6ec2c1a246004f1a2f69d25b7ec0
root <root@DESKTOP-U623PMG.localdomain>
2020-09-30 10:32:09 +0200

    create branch interetPage

--- jc_app.js
+++ jc_app.js
@@ -3,7 +3,7 @@ const mobileBurger = document.querySelector('.mobile-menuOpen');
 const mobileBurgerButton = document.querySelector('#burger-buttom');
 
 
-document.body.addEventListener('click', (e) => {
+document.addEventListener('click', (e) => {
     if (e.target !== mobileBurger && e.target !== mobileBurgerButton && mobileBurger.style.display === 'flex') { mobileBurger.style.display = 'none'};
 });
 
