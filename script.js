1fdfa4e6e5ce952b4a36b4686b1ac805d8502320
Will142 <will142@hotmail.fr>
2020-10-01 09:02:00 +0200

    a new day

--- script.js
+++ script.js
@@ -16,17 +16,3 @@ mobileBurgerButton.addEventListener('click', () => {
 
 
 
-const cards = document.querySelector(".cards");
-
-const eventList = [
-  {
-    name: "Ballets de Monte-Carlo, Monaco",
-    picture: "https://cdm0lfbn.cloudimg.io/v7/_origin_/image_uploader/photos/bd/original/ballets-de-monte-carlo-at-grimaldi-forum-monaco-monaco-2.jpg?p=large"
-  },
-  {
-    name: "Monte-Carlo Comedy Film Festival 2020",
-    picture: "https://cdm0lfbn.cloudimg.io/v7/_origin_/image_uploader/photos/11/original/monte-carlo-comedy-film-festival-2020-monte-carlo-monte-carlo.jpg?p=large"
-  },
- 
-];
-
